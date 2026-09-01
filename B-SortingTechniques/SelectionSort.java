91594f9eaf3eee25d2ebeb5da8f304a8e7b61719
Deelaksha NJ <deelaksha22@gmail.com>
2026-09-01 08:41:14 +0530

    DSA problems

---
+++ B-SortingTechniques/SelectionSort.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class SelectionSort {
+    public static void main(String[] args){
+        int [] a = {10,8,2,50,20};
+        int i, j, min, temp;
+        for(i=0;i<a.length -1;i++){
+            min = i;
+            for(j = i;j < a.length;j++){
+                if(a[j] < a[min])
+                    min = j;
+            }
+            temp = a[min];
+            a[min] = a[i];
+            a[i] = temp;
+        }
+        for(int x:a){
+            System.out.print(x + " ");
+        }
+    }
+    
+}
